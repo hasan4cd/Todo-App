5f98991e4a5ee59ce110cb48c988b332413f2d13
hasan4cd <hasan4cd@gmail.com>
2026-02-05 15:23:55 +0500

    Work Complete

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNbqVNVIa8B1MP1B2FzO4lz3Vj1rGlPt0",
+  authDomain: "todo-d2c37.firebaseapp.com",
+  projectId: "todo-d2c37",
+  storageBucket: "todo-d2c37.firebasestorage.app",
+  messagingSenderId: "131468815833",
+  appId: "1:131468815833:web:bc5522a59cb5ab95e2fdc8",
+  measurementId: "G-F0NLQF4L1F"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var db = firebase.database();
--- script.js
+++ script.js
@@ -1,36 +1,53 @@
 let input = document.getElementById("inputField");
 let list = document.getElementById("todoList");
-let edit = null;
+let editId = null;
+
 
 function addTodo() {
   if (input.value === "") return;
-
-  if (edit) {
-    edit.firstChild.textContent = input.value;
-    edit = null;
+  
+  if (editId) {
+    db.ref("todos/" + editId).update({
+      text: input.value
+    });
+    editId = null;
     input.value = "";
     return;
   }
 
-  let li = document.createElement("li");
-  let span = document.createElement("span");
-  span.textContent = input.value;
-
-  let editBtn = document.createElement("button");
-  editBtn.innerText = "Edit";
-  editBtn.onclick = () => {
-    input.value = span.textContent;
-    edit = li;
-  };
-
-  let delBtn = document.createElement("button");
-  delBtn.innerText = "Delete";
-  delBtn.onclick = () => li.remove();
-
-  li.appendChild(span);
-  li.appendChild(editBtn);
-  li.appendChild(delBtn);
-  list.appendChild(li);
+  db.ref("todos").push({
+    text: input.value
+  });
 
   input.value = "";
-}  
+}
+
+db.ref("todos").on("value", function(snapshot) {
+  list.innerHTML = "";
+  snapshot.forEach(function(childSnapshot) {
+    let id = childSnapshot.key;
+    let data = childSnapshot.val();
+
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    span.textContent = data.text;
+
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "Edit";
+    editBtn.onclick = function () {
+      input.value = data.text;
+      editId = id;
+    };
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "Delete";
+    delBtn.onclick = function () {
+      db.ref("todos/" + id).remove();
+    };
+
+    li.appendChild(span);
+    li.appendChild(editBtn);
+    li.appendChild(delBtn);
+    list.appendChild(li);
+  });
+});
